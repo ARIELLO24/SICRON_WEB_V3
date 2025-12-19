3f675e767d99d7169593875d4d0c0d02b9b51b2b
ARIEL LORENZ <04529620050@senacrs.edu.br>
2025-12-18 21:34:20 -0300

    Change API BaseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'http://sicronweb-backend-bi5j.onrender.com/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
